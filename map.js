93e2a34b157b80f41f46330826333ec7195142a7
hrecht <hrecht@users.noreply.github.com>
2015-08-13 17:23:55 -0400

    new header floating on map

--- map.js
+++ map.js
@@ -5,11 +5,11 @@
          maxZoom: 10,
          minZoom: 4
      })
-     .setView([39.623, -93.120], 5);
+     .setView([39.174360, -93.113837], 5);
  map.attributionControl
      .addAttribution('<a href="http://www.urban.org/">&copy; Urban Institute</a>');
 
- L.mapbox.tileLayer('urbaninstitute.6v96sml4').addTo(map);
+L.mapbox.tileLayer('urbaninstitute.6v96sml4').addTo(map);
  var gridLayer = L.mapbox.gridLayer('urbaninstitute.6v96sml4').addTo(map);
  var gridControl = L.mapbox.gridControl(gridLayer, {
          follow: true
